64377c408c73bc89f4e5768bb3a4e9f02ff2020b
Antonio Natusch <anthony2004951@gmail.com>
2024-12-20 20:33:49 -0400

    fix: fix missing login compatibility with other objets

--- js/login.js
+++ js/login.js
@@ -6,9 +6,17 @@ document.addEventListener('DOMContentLoaded', () => {
     role: 'Gerente Administrativo',
   };
 
+  // Inicializar cuentas predeterminadas si no existen
   if (!localStorage.getItem('accounts')) {
     localStorage.setItem('accounts', JSON.stringify([defaultAccount]));
   }
+
+  // Recuperar datos de empleados si existen
+  const getEmployees = () =>
+    JSON.parse(localStorage.getItem('empleados')) || [];
+
+  // Recuperar datos de cuentas si existen
+  const getAccounts = () => JSON.parse(localStorage.getItem('accounts')) || [];
 
   const loginForm = document.getElementById('login-form');
   loginForm.addEventListener('submit', (e) => {
@@ -16,14 +24,25 @@ document.addEventListener('DOMContentLoaded', () => {
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
 
-    const accounts = JSON.parse(localStorage.getItem('accounts'));
+    // Verificar tanto en el objeto 'accounts' como 'empleados'
+    const accounts = getAccounts();
+    const employees = getEmployees();
+
     const account = accounts.find(
       (acc) => acc.username === username && acc.password === password,
     );
 
+    const employee = employees.find(
+      (emp) => emp.usuario === username && emp.contraseña === password,
+    );
+
     if (account) {
       localStorage.setItem('currentUser', JSON.stringify(account));
-      alert('Inicio de sesión exitoso');
+      alert('Inicio de sesión exitoso (Cuentas)');
+      window.location.href = '../index.html';
+    } else if (employee) {
+      localStorage.setItem('currentUser', JSON.stringify(employee));
+      alert('Inicio de sesión exitoso (Empleados)');
       window.location.href = '../index.html';
     } else {
       alert('Credenciales incorrectas');
